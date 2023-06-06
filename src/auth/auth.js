2691a37fc2d7a3154c34abfc95bdfe1bb29323c4
Yasar195 <imyasar07@gmail.com>
2023-06-06 21:44:52 +0530

    update

--- src/auth/auth.js
+++ src/auth/auth.js
@@ -36,12 +36,11 @@ router.get('/', async (req, res) => {
     const user = new Promise((resolve, reject) => {
         const now = new Date()
         const date = now.toLocaleDateString()
-        const time = now.toLocaleTimeString()
         connection.query(`select users.user_name, users.user_type, users.user_email, users.registered, users.dep_id, users.branch_id, departments.dep_name, departments.dep_image, branches.branch_name from users join departments on users.dep_id = departments.dep_id join branches on users.branch_id = branches.branch_id where users.user_id='${res.locals.uid}';`, (err, result)=> {
             if(err){
                 reject()
             }
-            connection.query(`insert into user_activity (user_id, activity, dep, time, date) values ('${res.locals.uid}', 'sign in', 'login', '${time}', '${date}');`, (err) => {
+            connection.query(`insert into user_activity (user_id, activity, dep, date) values ('${res.locals.uid}', 'sign in', 'login', '${date}');`, (err) => {
                 if(err){
                     reject()
                 }
