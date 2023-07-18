fb4670d193de41d9804674d09ab4c783f9537ed7
Yasar195 <imyasar07@gmail.com>
2023-07-18 16:39:48 +0530

    update

--- src/leaves/leaves.js
+++ src/leaves/leaves.js
@@ -4,9 +4,12 @@ const router = require('express').Router()
 router.post('/', async (req, res) => {
     const data = req.body
     const result = new Promise((resolve, reject) => {
-        if(data.reason && data.apply_date && data.return_date && data.dep_id && data.branch_id){
-            connection.query(`insert into leave_request (user_id, apply_date, comeback_date, reason, dep_id, branch_id) values ('${res.locals.uid}', '${data.apply_date}', '${data.return_date}', '${data.reason}', ${data.dep_id}, ${data.branch_id});`, (err)=> {
-                err ? reject(): resolve()
+        if(data.reason && data.start_date && data.dep_id && data.branch_id){
+            connection.query(`insert into leave_request (user_id, start_date, reason, dep_id, branch_id ${data.end_date? `, end_date`: ''}) values ('${res.locals.uid}', '${data.start_date}', '${data.reason}', ${data.dep_id}, ${data.branch_id} ${data.end_date? `, '${data.end_date}'`: ''});`, (err)=> {
+                if(err){
+                    reject()
+                }
+                resolve()
             })
         }
         else{
@@ -52,9 +55,8 @@ router.get('/mystatus', (req, res) => {
 router.get('/', (req, res)=> {
     const result = new Promise((resolve, reject) => {
         if(req.query.dep_id){
-            connection.query(`select leave_request.leave_id, users.user_name, leave_request.apply_date, leave_request.comeback_date, leave_request.reason from leave_request join users on leave_request.user_id = users.user_id where leave_request.dep_id=${req.query.dep_id} and leave_request.status='pending';`, (err, response)=> {
+            connection.query(`select leave_request.leave_id, users.user_name, leave_request.apply_date, leave_request.start_date, leave_request.reason, leave_request.end_date from leave_request join users on leave_request.user_id = users.user_id where leave_request.dep_id=${req.query.dep_id} and leave_request.status='pending';`, (err, response)=> {
                 if(err){
-                    console.log(err)
                     reject()
                 }
                 resolve(response.rows)
