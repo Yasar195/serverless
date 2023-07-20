9db9adae96f395d24ee57fa83efeffbcce6e9c4e
Yasar195 <imyasar07@gmail.com>
2023-07-20 14:06:37 +0530

    update

--- src/bookings/bookings.js
+++ src/bookings/bookings.js
@@ -5,34 +5,22 @@ const { generateRandomString } = require('../utils/utils')
 
 router.post('/', (req, res)=> {
     const data = req.body
-    const file = req.files.pdf
     const now = new Date()
     const date = now.toLocaleDateString()
-    const key = `${generateRandomString(10)}.pdf`
-    const params = {
-        Bucket: 'travelitinerary',
-        Key: key,
-        Body: file.data,
-    };
     const upload = new Promise((resolve, reject)=> {
-        s3.upload(params, function (err) {
-            if (err) {
-              reject()
+        connection.query(`update customers set booked=true where customer_id=${data.customer_id};`, (err)=> {
+            if(err){
+                reject()
             }
-            connection.query(`update customers set booked=true where customer_id=${data.customer_id};`, (err)=> {
+            connection.query(`update users set points=${data.points} where user_id='${data.user_id}';`, (err)=> {
                 if(err){
                     reject()
                 }
-                connection.query(`update users set points=${data.points} where user_id='${data.user_id}';`, (err)=> {
+                connection.query(`insert into bookings (booking_date, customer_id, user_id, dep_id, branch_id) values ('${date}', ${data.customer_id}, '${data.user_id}', ${data.dep_id}, ${data.branch_id});`, (err)=> {
                     if(err){
                         reject()
                     }
-                    connection.query(`insert into bookings (booking_date, customer_id, user_id, amount_payable, amount_paid, travel_itinerary, dep_id, branch_id, staff_id) values ('${date}', ${data.customer_id}, '${data.user_id}', '${data.amount_payable}', '${data.amount_paid}', '${key}', ${data.dep_id}, ${data.branch_id}, '${data.staff_id}');`, (err)=> {
-                        if(err){
-                            reject()
-                        }
-                        resolve()
-                    })
+                    resolve()
                 })
             })
         })
@@ -92,7 +80,7 @@ router.get('/', (req, res)=> {
 
 router.get('/staff', (req, res)=> {
     const result = new Promise((resolve, reject)=> {
-        connection.query(`select bookings.booking_id, users.user_name, bookings.booking_date, customers.customer_name, bookings.travel_itinerary from bookings join users on bookings.user_id=users.user_id join customers on bookings.customer_id=customers.customer_id where bookings.staff_id='${res.locals.uid}';`, (err, response)=> {
+        connection.query(`select bookings.booking_id, users.user_name, bookings.booking_date, customers.customer_name from bookings join users on bookings.user_id=users.user_id join customers on bookings.customer_id=customers.customer_id where bookings.staff_id='${res.locals.uid}';`, (err, response)=> {
             if(err){
                 reject()
             }
@@ -101,15 +89,6 @@ router.get('/staff', (req, res)=> {
     })
 
     result.then((data)=> {
-        data.forEach(booking => {
-            const url = s3.getSignedUrl('getObject', {
-                Bucket: 'travelitinerary',
-                Key: booking.travel_itinerary,
-                Expires: 60 * 60
-            });
-            booking.travel_itinerary = url
-        })
-        
         res.status(200).json({
             result: data,
             success: true
@@ -156,7 +135,7 @@ router.put('/', (req, res)=> {
 
 router.get('/:id', (req, res)=> {
     const result = new Promise((resolve, reject)=> {
-        connection.query(`select bookings.booking_id, users.user_name, bookings.booking_date, customers.customer_name, bookings.amount_paid, bookings.amount_payable, bookings.travel_itinerary from bookings join users on bookings.user_id=users.user_id join customers on bookings.customer_id=customers.customer_id where bookings.booking_id=${req.params.id};`, (err, response)=> {
+        connection.query(`select bookings.booking_id, users.user_name, bookings.booking_date, customers.customer_name from bookings join users on bookings.user_id=users.user_id join customers on bookings.customer_id=customers.customer_id where bookings.booking_id=${req.params.id};`, (err, response)=> {
             if(err){
                 reject()
             }
@@ -165,15 +144,6 @@ router.get('/:id', (req, res)=> {
     })
 
     result.then((data)=> {
-        data.forEach(booking => {
-            const url = s3.getSignedUrl('getObject', {
-                Bucket: 'travelitinerary',
-                Key: booking.travel_itinerary,
-                Expires: 60 * 60
-            });
-            booking.travel_itinerary = url
-        })
-        
         res.status(200).json({
             result: data,
             success: true
