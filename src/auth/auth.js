3a48e0e2d02cb2913b286df510741b179d0903b1
Yasar195 <imyasar07@gmail.com>
2023-07-29 12:28:48 +0530

    update

--- src/auth/auth.js
+++ src/auth/auth.js
@@ -34,20 +34,11 @@ const authenticate = async (req, res, next) => {
 
 router.get('/', async (req, res) => {
     const user = new Promise((resolve, reject) => {
-        const now = new Date()
-        const date = now.toLocaleDateString()
         connection.query(`select users.user_id, users.user_name, users.profile_key, users.user_type, users.user_email, users.registered, users.dep_id, users.branch_id, departments.dep_name, departments.dep_image, departments.dep_color, branches.branch_name from users join departments on users.dep_id = departments.dep_id join branches on users.branch_id = branches.branch_id where users.user_id='${res.locals.uid}';`, (err, result)=> {
             if(err){
-                console.log(err)
                 reject()
             }
-            connection.query(`insert into user_activity (user_id, activity, dep, date) values ('${res.locals.uid}', 'sign in', 'login', '${date}');`, (err) => {
-                if(err){
-                    console.log(err)
-                    reject()
-                }
-                resolve(result.rows)
-            })
+            resolve(result.rows)
         })
     })
     user.then(async (data)=> {
