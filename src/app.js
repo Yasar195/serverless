12b384555780d202abd8e8bab8511d5ffdf00fba
Yasar195 <imyasar07@gmail.com>
2023-08-13 09:46:07 +0530

    optimised code

--- src/app.js
+++ src/app.js
@@ -8,7 +8,6 @@ const branches = require('./branch/branch');
 const leads = require('./leads/leads');
 const staff = require('./staff/staff');
 const tours = require('./tours/tours');
-const pdf = require('./pdf/pdf');
 const audio = require('./audio/audio')
 const leaves = require('./leaves/leaves')
 const rooms = require('./rooms/rooms')
@@ -72,7 +71,6 @@ app.use('/auth', router)
 app.use('/leads', leads)
 app.use('/staffs', staff)
 app.use('/tours', tours)
-app.use('/pdf', pdf)
 app.use('/audio', audio)
 app.use('/leaves', leaves)
 app.use('/rooms', rooms)
