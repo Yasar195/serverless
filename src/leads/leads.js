9870d7044eb4731f1993c8c5c07273b391f9fe9d
Yasar195 <imyasar07@gmail.com>
2023-09-27 14:28:33 +0530

    bug fix

--- src/leads/leads.js
+++ src/leads/leads.js
@@ -84,9 +84,29 @@ router.get('/followup', (req, res)=> {
     })
 })
 
-router.get('/', async (req, res) => {
+router.get('/fresh', async (req, res) => {
     const result = new Promise((resolve, reject)=> {
-        connection.query(`select * from customers join leads on customers.customer_id=leads.customer_id where leads.user_id = '${res.locals.uid}' and leads.follow_up=false limit 10 offset ${req.query.page? `${(parseInt(req.query.page) - 1)*10}`: '0'};`, (err, result)=> {
+        connection.query(`select * from customers join leads on customers.customer_id=leads.customer_id where leads.user_id = '${res.locals.uid}' and leads.follow_up=false and customers.customer_progress='Not started' limit 10 offset ${req.query.page? `${(parseInt(req.query.page) - 1)*10}`: '0'};`, (err, result)=> {
+            err? reject(): resolve(result.rows)
+        })
+    })
+    result.then((data)=> {
+        res.status(200).json({
+            result: data,
+            success: true
+        })
+    })
+    .catch(()=> {
+        res.status(500).json({
+            result: "fetching leads failed",
+            success: false
+        })
+    })
+})
+
+router.get('/old', async (req, res) => {
+    const result = new Promise((resolve, reject)=> {
+        connection.query(`select * from customers join leads on customers.customer_id=leads.customer_id where leads.user_id = '${res.locals.uid}' and leads.follow_up=false and customers.customer_progress!='Not started' limit 10 offset ${req.query.page? `${(parseInt(req.query.page) - 1)*10}`: '0'};`, (err, result)=> {
             err? reject(): resolve(result.rows)
         })
     })
