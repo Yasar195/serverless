5d77f4957643540abf1cc0cc2345fe9f42f2bf4c
Yasar195 <imyasar07@gmail.com>
2023-09-28 11:50:00 +0530

    feature

--- src/audio/audio.js
+++ src/audio/audio.js
@@ -84,7 +84,7 @@ router.post('/upload', (req, res)=> {
 router.get('/', (req, res)=> {
     const audios = new Promise((resolve, reject)=> {
         if(req.query.dep_id&&req.query.branch_id){
-            connection.query(`select * from customer_response join users on customer_response.user_id = users.user_id join customers on customer_response.customer_id=customers.customer_id where customers.branch_id=${req.query.branch_id} and customers.dep_id=${req.query.dep_id} ${req.query.user_id? `and users.user_id='${req.query.user_id}'`: ''} ${req.query.id? `and customers.customer_id=${req.query.id}`: ''} ${req.query.name? `and lower(customers.customer_name) like lower('%${req.query.name}%')`: ''} order by customer_response.response_id desc limit 10 offset ${req.query.page? `${(parseInt(req.query.page) - 1)*10}`: '0'};;`, (err, result)=> {
+            connection.query(`select * from customer_response join users on customer_response.user_id = users.user_id join customers on customer_response.customer_id=customers.customer_id where customers.branch_id=${req.query.branch_id} and customers.dep_id=${req.query.dep_id} ${req.query.user_id? `and users.user_id='${req.query.user_id}'`: ''} ${req.query.id? `and customers.customer_id=${req.query.id}`: ''} ${req.query.name? `and lower(customers.customer_name) like lower('%${req.query.name}%')`: ''} order by customer_response.response_id desc limit 10 offset ${req.query.page? `${(parseInt(req.query.page) - 1)*10}`: '0'};`, (err, result)=> {
                 err? reject(): resolve(result.rows)
             })
         }
@@ -121,7 +121,7 @@ router.get('/', (req, res)=> {
 router.get('/:id', (req, res)=> {
     const audios = new Promise((resolve, reject)=> {
         if(req.params.id){
-            connection.query(`select response_text, call_date from customer_response where customer_id=${req.params.id};`, (err, result)=> {
+            connection.query(`select response_text, call_date from customer_response where customer_id=${req.params.id} order by response_id desc limit 10 offset ${req.query.page? `${(parseInt(req.query.page) - 1)*10}`: '0'};`, (err, result)=> {
                 err? reject(): resolve(result.rows)
             })
         }
