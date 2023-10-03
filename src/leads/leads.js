af6b03f39c69b404b0b3c5fde3b972704828cc3a
Yasar195 <imyasar07@gmail.com>
2023-10-03 20:58:01 +0530

    update

--- src/leads/leads.js
+++ src/leads/leads.js
@@ -66,7 +66,7 @@ router.get('/dash/followup', (req, res)=> {
 
 router.get('/followup', (req, res)=> {
     const result = new Promise((resolve, reject)=> {
-        connection.query(`select * from customers join leads on customers.customer_id=leads.customer_id where leads.user_id = '${res.locals.uid}' and leads.follow_up=true ${req.query.name? `and lower(customers.customer_name) like lower('%${req.query.name}%')`: ''} ${req.query.id? `and customers.customer_id=${req.query.id}`: ''};`, (err, result)=> {
+        connection.query(`select * from customers join leads on customers.customer_id=leads.customer_id where leads.user_id = '${res.locals.uid}' and leads.follow_up=true and leads.follow_up_date<=current_date ${req.query.name? `and lower(customers.customer_name) like lower('%${req.query.name}%')`: ''} ${req.query.id? `and customers.customer_id=${req.query.id}`: ''} limit 10 offset ${req.query.page? `${(parseInt(req.query.page) - 1)*10}`: '0'};`, (err, result)=> {
             err? reject(): resolve(result.rows)
         })
     })
